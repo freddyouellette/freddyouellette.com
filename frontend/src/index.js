75868c36a5c153baad4a56f5471fd1530f06a002
Frederick Ouellette <freddy.ouellette@gmail.com>
2018-08-26 09:12:59 -0500

    Pushed registerServiceWorker to utilities directory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './custom-bootstrap.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './utilities/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
